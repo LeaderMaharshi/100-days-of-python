98ac9305005196f80cc815134f61b98dffe1c3c8
LeaderMaharshi <leadermaharshi@gmail.com>
2022-11-11 18:02:01 +0530

    Day-1

---
+++ Day-1/day-1.py
@@ -0,0 +1,10 @@
+# Bandname generator
+
+#greeting for your program
+print("Welcome to the band name generator")
+#Ask user for the city that they grew up in?
+city_name = input("What's name of the city you grew up in?")
+#ask user for the name of a pet
+pet_name = input("what is your pet name?")
+# combine city_name and pet_name
+print("Your band name is: " + city_name + " " + pet_name)
