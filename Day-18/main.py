89b50b79a389171cfed12359dc678a9ee07b4e02
LeaderMaharshi <leadermaharshi@gmail.com>
2023-01-18 18:21:57 +0530

    Turtle starting

---
+++ Day-18/main.py
@@ -0,0 +1,24 @@
+import turtle
+
+timmy = turtle.Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+
+# Dreaw a square
+
+n = 100
+    
+timmy.forward(n)
+timmy.left(90)
+timmy.forward(n)
+timmy.left(90)
+timmy.forward(n)
+timmy.left(90)
+timmy.forward(n)
+timmy.left(90)
+ 
+    
+
+
+screen = turtle.Screen()
+screen.exitonclick()
