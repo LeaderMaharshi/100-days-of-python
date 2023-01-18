b594c2b848fe87d10259e6dc59c2276e7d6c9d70
LeaderMaharshi <leadermaharshi@gmail.com>
2023-01-18 18:46:33 +0530

    Turtle practice

--- Day-18/main.py
+++ Day-18/main.py
@@ -1,22 +1,30 @@
 import turtle
 
-timmy = turtle.Turtle()
-timmy.shape("turtle")
-timmy.color("red")
+# timmy = turtle.Turtle()
+# timmy.shape("turtle")
+# timmy.color("red")
 
 # Dreaw a square
 
-n = 100
+# n = 100
     
-timmy.forward(n)
-timmy.left(90)
-timmy.forward(n)
-timmy.left(90)
-timmy.forward(n)
-timmy.left(90)
-timmy.forward(n)
-timmy.left(90)
+# timmy.forward(n)
+# timmy.left(90)
+# timmy.forward(n)
+# timmy.left(90)
+# timmy.forward(n)
+# timmy.left(90)
+# timmy.forward(n)
+# timmy.left(90)
  
+# Draw dash lines
+timmy = turtle.Turtle()
+
+for _ in range(15):
+    turtle.forward(10)
+    turtle.penup()
+    turtle.forward(10)
+    turtle.penup()
     
 
 
